2834dd77c428d4db05d8f36fabc393d0308e78c2
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-07-31 07:13:33 +0300

    Последние задачи решены

--- 6_Collections/6_10_chainmap.py
+++ 6_Collections/6_10_chainmap.py
@@ -99,6 +99,130 @@ print('-' * line_length)
 print(f"ИТОГ: {total}р")
 '''
 
-# 
+# Теория ChainMap 2
+'''
+Все словари объекта ChainMap хранятся во внутреннем списке
+Этот спиосок можно получить через аттрибут maps
+'''
+
+'''
+from collections import ChainMap
+
+for_adoption = {'dogs': 15, 'cats': 8, 'pythons':9}
+vet_treatment = {'dogs': 7, 'cats': 2, 'tigers': 3}
+
+pets = ChainMap(for_adoption, vet_treatment)
+print(pets.maps) # Получение списка словарей
+
+# Над этим списком можно проводить операции
+pets.maps.reverse()
+pets.maps[0]['lions'] = 10
+del pets.maps[1]['cats']
+
+print(pets.maps)
+
+# Используя аттрибут maps можно ИЗМЕНЯТЬ все словари ChainMap
+# Также можно провести полную иттерацию, а не по уникальным 
+
+for animals in pets.maps:
+    for key, value in animals.items():
+        print(key, '->', value)
+'''
+
+# Метод new_child()
+'''
+# ChainMap с новым словарём по 0 индексу
+# Если () - пустой словарь
+# Если ({...}) - переданный словарь
+
+from collections import ChainMap
+
+dad = {'name': 'Timur', 'age': 29}
+mom = {'name': 'Rosaly', 'age': 28}
+
+old_family = ChainMap(dad, mom)
+
+son = {'name': 'Solan', 'age': 0}
+
+print(type(old_family.new_child(son)))
+# Метод new_child() - возращает изменённый Объект ChainMap 
+# Он НЕ изменяет вызывающий!
+new_family = old_family.new_child(son)
+
+print(old_family)
+'''
+
+# Атрибут parents
+'''
+# Возращает ChainMap, без 1 словаря.
+# Полезно, для пропуска 1ого словаря при поиске ключей
+
+from collections import ChainMap
+
+dad = {'name': 'Timur', 'age': 29}  # 2
+mom = {'name': 'Rosaly', 'age': 28} # 3
+son = {'name': 'Soslan', 'age': 0}  # 1
+
+family = ChainMap(son, dad, mom)
+
+print(family)
+family = family.parents
+print(family)
+print(type(family.parents))
+'''
+
+# Как производится сравнение ChainMap-ов
+'''
+from collections import ChainMap
+
+cm1 = ChainMap({'a': 10, 'b': 20})
+cm2 = ChainMap({'b': 20, 'a': 10})
+
+print(cm1 == cm2)
+print(dict(cm1.items()) == dict(cm2.items()))
+'''
+
+# Функция get_all_values()
+'''
+from collections import ChainMap
+
+def get_all_values(chainmap: ChainMap, key):
+    answer = set()
+
+    # Провести полную иттерации (по всем словорям) используя .maps
+    for dict in chainmap.maps:
+        for iter_key, value in dict.items():
+            if key == iter_key:
+                answer.add(value)
+    
+    return answer
+
+chainmap = ChainMap({'name': 'Arthur'}, {'name': 'Timur'})
+result = get_all_values(chainmap, 'name')
+
+print(*sorted(result))
+# Arthur Timur
+'''
+
+# deep_update()
+'''
+
+from collections import ChainMap
+
+def deep_update(chainmap, key, value):
+    if key in chainmap:
+        [dct.update({key: value}) for dct in chainmap.maps if key in dct]
+    else:
+        chainmap[key] = value
+'''
+
+# get_value()
 '''
 '''
+
+from collections import ChainMap
+
+def get_value(chainmap, key, from_left=True):
+    return chainmap.get(key) if from_left else ChainMap(*chainmap.maps[::-1]).get(key)
+
+
