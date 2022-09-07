3ea2ddbe27bf6a6edf160ae9046062d63acbbe9e
Gleb-Kolonin <topsecretway@yandex.ru>
2022-09-07 15:15:18 +0300

    Изучение регулярных выражений

--- 11_RegEx/RegEx_1.py
+++ 11_RegEx/RegEx_1.py
@@ -1,4 +1,3 @@
-
 # Я в аду?
 '''
 Напишите программу, в которая в заданном тексте находит все телефонные номера, соответствующие следующим форматам:
@@ -17,8 +16,61 @@
 # #7-123-123-11-22
 # #8-123-1231-1221
 
-pattern = r'7|8-\d{3}-\d{3}-\d{2}-\d{2}'
 
-i = input()
+'''
+from re import search
+pattern1 = r'(7-\d{3}-\d{3}-\d{2}-\d{2})'
+pattern2 = r'(8-\d{3}-\d{4}-\d{4})'
 
-# Как применить мой паттерн?
+inp = input()
+# inp = "Перезвони мне, пожалуйста: 7-919-667-21-19"
+# inp = "Артур: +7-919-667-21-19, Анри: 7-hey-anri-anri, Тимур: 8-917-4864-1911"
+phones = []
+match = True
+
+while True:
+    match1 = search(pattern1, inp)
+    match2 = search(pattern2, inp)
+
+    if match1 == None and match2 == None:
+        break
+    elif match1 != None and match2 == None:
+        match = match1
+    elif match1 == None and match2 != None:
+        match = match2
+    else:
+        if match1.start() < match2.start():
+            match = match1
+        else:
+            match = match2
+    
+    phones.append(inp[match.start():match.end()])
+
+    inp = inp[match.end():]
+  
+print(*phones, sep="\n")
+'''
+
+# Более элегантный вариант
+'''
+import re
+
+st = input()
+regex = r'7-\d{3}-\d{3}-\d{2}-\d{2}|8-\d{3}-\d{4}-\d{4}'
+reg = re.findall(regex, st)
+
+print(*reg, sep='\n')
+'''
+
+# 
+'''
+'''
+import re
+
+st = "I have these files: caaat.png, acab.txt, cat.jpeg, tca-ca.txt, na1.csv, ca2.csv"
+regex = r'ca[a-e]|ca[1-5]'
+reg = re.findall(regex, st)
+
+print(len(reg))
+print(*reg, sep='\n')
+
--- 11_RegEx/module_re_6.py
+++ 11_RegEx/module_re_6.py
@@ -1,26 +1,95 @@
-from re import search
-
 # search возращает позицию первого удачного поиска.
+# from re import search
+'''
+print(search('super', 'superstition again superstition')) # span(0, 5) - индекс начального и конечного вхождения (невключительно)
+print(search('super', 'without')) # None - не найдено
 
-match1 = search('super', 'superstition again superstition')
-match2 = search('super', 'without')
-
-print(match1) # span(0, 5) - индекс начального и конечного вхождения (невключительно)
-print(match2) # None - не найдено
-
-match3 = search('[a-z]+', '123foFo456')
-match4 = search('\d+', 'foo.bar')
-
-print(match3)
-print(match4)
+print(search('[a-z]+', '123foFo456'))
+print(search('\d+', 'foo.bar'))
+'''
 
 # match - начало строки должно совпадать с паттерном
+# from re import match
+'''
+print(match('super', 'superstition'))
+print(match('super', 'insuperable'))
+'''
 
-from re import match
+# fullmatch - если вся строка соответствует шаблону
+# from re import fullmatch
+'''
+print(fullmatch('\d+', '123foo'))
+print(fullmatch('\d+', '123321'))
+'''
 
-match5 = match('super', 'superstition')
-match6 = match('super', 'insuperable')
+# Удобно использовать при валидации данных
+'''
+phone_pattern = r'\d{3}-\d{3}-\d{4}'
+print(fullmatch(phone_pattern, '777-888-1234'))
+'''
 
-print(match5)
-print(match6)
+# Полезные методы и атрибуты типа Match
+
+# Метод group() - возращает одну или несколько подгрупп совпадения.
+'''
+from re import search
+# () - вложенное выражение
+match = search('(\w+),(\w+),(\w+)', 'foo,bar,baz')
+
+print(match)
+
+print(match.group())  # все группы совпадения
+print(match.group(0)) # 0 - тоже все группы совпадения
+print(match.group(1)) # 1ая подгруппа
+
+print(match.group(1, 2, 3)) # кортеж из подгрупп
+print(match.group(1, 2, 3, 1, 2, 2, 3, 3, 3, 3)) # Можно выводить захваченные группы в любом порядке любое число раз
+'''
+
+# Удобство работы с именованными группами
+'''
+from re import search
+
+match = search('(?P<w1>\w+),(?P<w2>\w+),(?P<w3>\w+)', 'foo,bar,baz')
+
+print(match.group())
+print(match.group('w1'))
+print(match.group('w1', 'w2', 'w3', 'w2', 'w3'))
+'''
+
+# Метод groups() - все захваченные группы в виде кортежа
+'''
+from re import search
+
+match = search('(\w+),(\w+),(\w+)?', 'foo,bar,') # ? - 0 или более вхождений последней группы
+print(match.groups()) # None - группа не участвовала в сопоставлении.
+print(match.groups(default='Not detected')) # Результат в случаях, когда не было сопоставления
+'''
+
+# Метод groupdict() - словарь всех захваченных именованных групп
+'''
+from re import search
+
+match = search('(?P<w1>\w+)?,(?P<w2>\w+)?,(?P<w3>\w+)', ',,baz')
+print(match.groupdict(default='not_detected'))
+'''
+
+# Методы start() и end() - возращают индексы кортежа span (охвата), где было найдено совпадение с RegEx 
+# Метод span() - возращает кортеж span (охвата), где было найдено совпадение с RegEx 
+
+
+# ============================================== Задачки ==============================================
+'''
+from re import search
+match1 = search(r'\w+', 'Hello, Timur') # \w+ = [a-zA-Z]. Запятая и последующие символы не войдут в совпадение
+# print(match1)
+print(match1.group() if match1 else None)
+'''
+
+# Телефонные номера
+'''
+План:
+
+Очевидно нужно использовать stdin
+'''
 
