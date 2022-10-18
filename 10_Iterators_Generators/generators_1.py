4f0cbb929bb295545db3cbe5adda794abc1e6299
Gleb-Kolonin <topsecretway@yandex.ru>
2022-10-18 07:32:38 +0300

    Генераторы

--- 10_Iterators_Generators/generators_1.py
+++ 10_Iterators_Generators/generators_1.py
@@ -1,13 +1,13 @@
 # Генераторы в Python делятся на 2 группы:
 '''
-1) Функции генераторы
-2) Выражения генераторы
+1) Функции генераторы - рассматриваются тут
+2) Выражения генераторы - рассматриваются в след. уроке
 '''
 
 # Функции генераторы - функция, возвращающая итератор. Использует yield вместо return
 # Функция генератор сохраняет лок. переменные от вызова к вызову. Т.е. явл. Возобновляемой функцией.
 
-# Пример функции-генератора
+########################################## Пример функции-генератора
 
 '''
 def generate_ints(n):
@@ -48,7 +48,70 @@ for num in iterator2:
     print(num)
 '''
 
-# 
+# Генераторная-функция simple_sequence()
+# Генерировать бесконечную последовательность с 1
+# Где каждое число встречается столько раз, каково оно
 '''
+def simple_sequence():
+    counter = 0
+    val = 1
+    while True:
+        while counter != val:
+            yield val
+            counter += 1
+        counter = 0
+        val += 1
+'''
+
+# Генераторная-функция alternating_sequence()
+'''
+def alternating_sequence(count = None):
+    val = 1
+    if count:
+        for _ in range(count):
+            yield val
+            val = abs(val) + 1
+            if val % 2 == 0:
+                val = -val
+        return StopIteration
+    while True:
+        yield val
+        val = abs(val) + 1
+        if val % 2 == 0:
+            val = -val
+'''
+
+# Функция primes()
+# Порождает последовательность простых числе от left до right
+'''
+def primes(left, right):
+    pass
+
+'''
+
+# Функция reverse()
+'''
+def reverse(sequence):
+    for elem in reversed(sequence):
+        yield elem
+'''
+
+
+# Функция dates()
+'''
+'''
+
+# Функция card_deck()
+# Порождает генератор колоды без масти suit
+'''
+def card_deck(suit: str):
+    card_values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "валет", "дама", "король", "туз"]
+    card_mast = ["пик", "треф", "бубен", "червей"]
+    card_mast.remove(suit)
+
+    while True:
+        for mast in card_mast:
+            for card in card_values:
+                yield f"{card} {mast}"
 '''
 
