de853592649976d60b14f36c85ac96d009f31221
Gleb-Kolonin <topsecretway@yandex.ru>
2022-11-05 14:37:54 +0300

    Решение задач на генераторы ч1

--- 10_Iterators_Generators/generators_1.py
+++ 10_Iterators_Generators/generators_1.py
@@ -83,11 +83,62 @@ def alternating_sequence(count = None):
 
 # Функция primes()
 # Порождает последовательность простых числе от left до right
-'''
-def primes(left, right):
-    pass
+# Определить является ли число простым можно исп. алгоритм Решето Эратосфена
+# Простое число - число деляющиеся без остатка только на себя и на 1 (кроме 1)
 
-'''
+"""
+def eratosphene(maximum):
+    # список заполняется значениями от 0 до n
+    a = []
+    for i in range(maximum + 1):
+        a.append(i)
+    
+    ''' В списке a индекс - это число. А его значение 0 - составное, 1 - простое '''
+
+    # Ноль и Один - не считают простым числом Т.е. они составные
+    a[1] = 0
+    
+    # начинаем с 3-го элемента, т.е. 2ки
+    i = 2
+    while i <= maximum:
+        # Если значение предыдущей ячейки != 0,
+        # в текущей ячейке содержится простое число.
+        if a[i] != 0:
+            # первое кратное ему
+            # будет в два раза больше
+            j = i + i
+            while j <= maximum:
+                # это число составное, поэтому заменяем его нулем
+                a[j] = 0
+                # переходим к следующему числу, которое кратно i
+                # (оно на i больше)
+                j = j + i
+        i += 1
+    
+    # избавляемся от всех нулей 
+    a = [el for el in a if el != 0]
+    return a
+
+def primes(left: int, right: int):
+    primary = eratosphene(right)
+
+    # Беру часть простых чисел в заданном диапозоне
+    for i in range(len(primary)):
+        if primary[i] >= left:
+            yield primary[i]
+
+
+generator = primes(1, 15)
+print(*generator)
+# 2 3 5 7 11 13
+
+generator = primes(6, 36)
+
+print(next(generator))
+print(next(generator))
+# 7
+# 11
+"""
 
 # Функция reverse()
 '''
@@ -96,9 +147,23 @@ def reverse(sequence):
         yield elem
 '''
 
-
 # Функция dates()
 '''
+from datetime import date
+from math import inf
+
+def dates(start: date, count = None):
+    if not count:
+        count = inf
+    
+    start = start.toordinal() - 1
+    while count > 0: # Пока не произойдёт ошибка получения максимальной даты
+        start += 1
+        count -= 1
+        try:
+            yield date.fromordinal(start)
+        except ValueError: # Ошибка получения максимальной даты
+            break
 '''
 
 # Функция card_deck()
